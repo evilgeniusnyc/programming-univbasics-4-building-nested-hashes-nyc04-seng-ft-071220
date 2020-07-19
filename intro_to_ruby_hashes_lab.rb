306877c33db2a212e085fbb44c0812984ddd7cc9
Steven Williams <evilgeniusnyc@me.com>
2020-07-19 20:06:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,4 @@
-#This method should do the following:
-
-return a hash with one key called :railroads that points to an empty hash
+#This method should do the following: return a hash with one key called :railroads that points to an empty hash
 
 def base_hash 
 {
@@ -13,8 +11,8 @@ end
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity
+  
 def monopoly_with_second_tier
-
  {
     railroads: {
       pieces: 4,
@@ -24,11 +22,19 @@ def monopoly_with_second_tier
   }
   
 end
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
+
+# When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+def monopoly_with_third_tier
+   {
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
+    }
+  }
 
 end
 
