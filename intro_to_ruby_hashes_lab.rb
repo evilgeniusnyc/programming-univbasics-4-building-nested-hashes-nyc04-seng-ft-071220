4d2a8fb6aad32dcaf6f65369b8d6ddd275f3566a
Steven Williams <evilgeniusnyc@me.com>
2020-07-19 20:07:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,10 @@ def monopoly_with_third_tier
    {
     railroads: {
       pieces: 4,
-      rent_in_dollars: {},
+      rent_in_dollars: {
+        
+        
+      },
       names: {}
     }
   }
